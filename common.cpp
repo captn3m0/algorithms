a4bf8049ec29b0ecafd4d223aba12d5135fd1173
Abhay Rana <capt.n3m0@gmail.com>
2013-02-01 17:15:32 +0530

    Fix assertions for sorted array. Add display function

--- common.cpp
+++ common.cpp
@@ -1,9 +1,18 @@
 #include <assert.h>
+#include <stdio.h>
 void assertSorted(int* a,int length)
 {
 	//Check that the entire array is in sorted order
 	for(int i=0;i<length-1;i++)
 	{
-		assert(a[i]<a[i+1]);
+		assert(a[i]<=a[i+1]);
+	}
+}
+void display(int* a,int length)
+{
+	//Check that the entire array is in sorted order
+	for(int i=0;i<length-1;i++)
+	{
+		printf("%d ",a[i]);
 	}
 }
